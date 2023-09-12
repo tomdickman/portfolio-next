19bc7ad363ffa4415789f2fd9f60f181ad6ffee1
Tom Dickman <twdickman@gmail.com>
2023-09-12 13:06:29 +1000

    feat: Create basic website structure
    Introduce some basic copy, styling and fonts.
    Add a responsive burger menu icon which is:
    - position middle bottom on mobile
    - position top left on tablet and desktop
    Hide the burger menu icon until menu content is developed

---
+++ src/components/burger/Burger.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import styles from "./styles.module.css"
+
+export type BurgerProps = {
+  open?: boolean
+}
+
+export const Burger = ({ open = false }) => {
+	return (
+		<svg
+			viewBox="0 0 10 10"
+			className={`${styles.burgerMenuIcon} ${open ? styles.open : ""}`}
+		>
+			<line
+				className={styles.burgerMenuIconTop}
+				fill="none"
+				x1="1"
+				y1="3"
+				x2="9"
+				y2="3"
+				stroke="currentcolor"
+			/>
+			<line
+				className={styles.burgerMenuIconMiddle}
+				fill="none"
+				x1="1"
+				y1="5"
+				x2="9"
+				y2="5"
+				stroke="currentcolor"
+			/>
+			<line
+				className={styles.burgerMenuIconBottom}
+				fill="none"
+				x1="1"
+				y1="7"
+				x2="9"
+				y2="7"
+				stroke="currentcolor"
+			/>
+		</svg>
+	)
+}
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Burger } from '../burger'
+import styles from './styles.module.css'
+import { useState } from 'react'
+
+export const Menu = () => {
+  const [open, setOpen] = useState(false)
+
+  const clickHandler = () => setOpen(current => !current)
+
+  return (
+    <div className={styles.menu}>
+      <button className={styles.button} onClick={clickHandler} >
+        <Burger open={open} />
+      </button>
+    </div>
+  )
+}
