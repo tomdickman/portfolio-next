e13feeb1a7a0ea8aba28ab61fef089f530b1c98f
Tom Dickman <twdickman@gmail.com>
2023-10-14 00:06:12 +1100

    fix: Remove ability to tab menu when closed

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -12,7 +12,6 @@ export const Menu = () => {
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (ref?.current && !ref.current.contains(event.target as Node)) {
-        console.log('Menu closed')
         setOpen(false)
       }
     }
@@ -35,13 +34,13 @@ export const Menu = () => {
       <nav className={styles.nav}>
         <ul className={styles.links}>
           <li>
-            <Link className={styles.link} href='/'>Home</Link>
+            <Link tabIndex={open ? 0 : -1} className={styles.link} href='/'>Home</Link>
           </li>
           <li>
-            <Link className={styles.link} href='/projects'>Projects</Link>
+            <Link tabIndex={open ? 0 : -1} className={styles.link} href='/projects'>Projects</Link>
           </li>
           <li>
-            <Link className={styles.link} href='/contact'>Contact Me</Link>
+            <Link tabIndex={open ? 0 : -1} className={styles.link} href='/contact'>Contact Me</Link>
           </li>
         </ul>
       </nav>
