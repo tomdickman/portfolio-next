4a199af43c9fba4667c39c53a10fb642b453ea52
Tom Dickman <twdickman@gmail.com>
2023-10-14 00:09:46 +1100

    chore: Fix spacing

--- src/components/burger/Burger.tsx
+++ src/components/burger/Burger.tsx
@@ -8,41 +8,41 @@ export type BurgerProps = {
 }
 
 export const Burger = ({
-	className = '',
-	open = false
+  className = '',
+  open = false
 }) => {
-	return (
-		<svg
-			viewBox="0 0 10 10"
-			className={`${className} ${styles.burgerMenuIcon} ${open ? styles.open : ""}`}
-		>
-			<line
-				className={styles.burgerMenuIconTop}
-				fill="none"
-				x1="1"
-				y1="3"
-				x2="9"
-				y2="3"
-				stroke="currentcolor"
-			/>
-			<line
-				className={styles.burgerMenuIconMiddle}
-				fill="none"
-				x1="1"
-				y1="5"
-				x2="9"
-				y2="5"
-				stroke="currentcolor"
-			/>
-			<line
-				className={styles.burgerMenuIconBottom}
-				fill="none"
-				x1="1"
-				y1="7"
-				x2="9"
-				y2="7"
-				stroke="currentcolor"
-			/>
-		</svg>
-	)
+  return (
+    <svg
+      viewBox="0 0 10 10"
+      className={`${className} ${styles.burgerMenuIcon} ${open ? styles.open : ""}`}
+    >
+      <line
+        className={styles.burgerMenuIconTop}
+        fill="none"
+        x1="1"
+        y1="3"
+        x2="9"
+        y2="3"
+        stroke="currentcolor"
+      />
+      <line
+        className={styles.burgerMenuIconMiddle}
+        fill="none"
+        x1="1"
+        y1="5"
+        x2="9"
+        y2="5"
+        stroke="currentcolor"
+      />
+      <line
+        className={styles.burgerMenuIconBottom}
+        fill="none"
+        x1="1"
+        y1="7"
+        x2="9"
+        y2="7"
+        stroke="currentcolor"
+      />
+    </svg>
+  )
 }
