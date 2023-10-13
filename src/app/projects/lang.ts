eb1b5309a616da137f89ce1a66a1732e44f85fd5
Tom Dickman <twdickman@gmail.com>
2023-10-13 23:27:11 +1100

    feat: Add images and amend project page text
    - Introduce language files for potential locatlisation

---
+++ src/app/projects/lang.ts
@@ -0,0 +1,6 @@
+export const EN = {
+  IRESS: 'Iress',
+  TARGET_IMAGE_ALT: 'Target image',
+  TARGET_LINK: 'Target Australia',
+  XPLAN_THUB_IMAGE_ALT: 'Xplan Transaction Hub image',
+}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,6 @@
+import { EN } from './lang'
+import Image from 'next/image'
+import Link from 'next/link'
 import styles from './styles.module.css'
 
 const Projects = () => {
@@ -5,26 +8,40 @@ const Projects = () => {
     <article>
       <h1>Projects</h1>
       <h2 className={styles.project_title}>2022-2023 Target Australia - UI Replatforming</h2>
+      <h3 className={styles.project_position}>Senior Software Engineer</h3>
       <div className={styles.project_description}>
         <ul className={styles.project_description_list}>
           <li>Creation of brand new Target Design System designed for performance (not built on pre-existing framework)</li>
-          <li>Implementation of a NextJS frontend focussed on performance</li>
+          <li>Implementation of a NextJS frontend focussed on performance and Search Engine Optimisation (SEO)</li>
           <li>Rollout of new platform alongside existing legacy platform to allow for staggered release and feature development</li>
-          <li>Zero downtime rollout and A/B testing</li>
-          <li>Add accessibility to Target website platform</li>
+          <li>Assist in implementation of zero downtime feature rollout and A/B testing</li>
+          <li>Add accessibility features and automated A11Y testing to Target UI platform</li>
         </ul>
+        <div className={styles.image_container}>
+          <Link href={'https://www.target.com.au/'} title={EN.TARGET_LINK}>
+            <Image style={{ width: '100%', height: 'auto' }} height={400} width={600} src={'/images/target.webp'} alt={EN.TARGET_IMAGE_ALT} />
+          </Link>
+        </div>
       </div>
       <h2 className={styles.project_title}>2021-2022 Iress - Transactions Hub Micro Frontend</h2>
+      <h3 className={styles.project_position}>Software Engineer</h3>
       <div className={styles.project_description}>
         <ul className={styles.project_description_list}>
           <li>Replacement of monolith platform page with a micro frontend</li>
           <li>Development of Backend for Frontend (BFF) to abstract API calls</li>
+          <li>Implementation of AG Grid for data display and management in UI</li>
           <li>Mounting of micro frontend in web component to prevent style conflicts</li>
-          <li>Development of user session state management between monolith and micro frontend</li>
-          <li>First project to use and ratify Iress Design System</li>
+          <li>Development of user session state management between monolith and micro frontend via a pub/sub model and event driven data</li>
+          <li>First project to use and ratify <a href='https://iress-design-system.aws-demo-production-au.iress.online/?path=/story/introduction-iress-design-system--page'>Iress Design System</a></li>
         </ul>
       </div>
+      <div className={styles.image_container}>
+        <Link href={'https://www.iress.com/'} title={EN.IRESS}>
+          <Image style={{ width: '100%', height: 'auto' }} height={400} width={600} src={'/images/xplan.webp'} alt={EN.XPLAN_THUB_IMAGE_ALT} />
+        </Link>
+      </div>
       <h2 className={styles.project_title}>2020-2021 Catalyst IT</h2>
+      <h3 className={styles.project_position}>Web Developer / Software Engineer</h3>
       <div className={styles.project_description}>
         <ul className={styles.project_description_list}>
           <li>Developed an open-source plugin for extracting metadata from existing resources in the Moodle LMS (Learning Management System), along with subplugins for extracting different metadata using Apache Tika and an open source readability API.</li>
